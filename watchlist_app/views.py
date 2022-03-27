02e7dd0086016072b0547405a6f3564bd76f32a6
sangnn <sangnn@vng.com.vn>
2022-03-27 22:21:28 +0700

    Return simple list of movies

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from watchlist_app.models import Movie
+from django.http import JsonResponse
 
-# Create your views here.
+# Complex QuerySet -> Python Dictionary -> JsonReponse
+def movie_list(request):
+    movies = Movie.objects.all() # QuerySet
+    data = {
+      'movies' : list(movies.values())  # Convert to PythonDict
+    }
+    
+    return JsonResponse(data)
