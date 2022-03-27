d75f162dd9065453d337f99cd348c5adf14dbca6
sangnn <sangnn@vng.com.vn>
2022-03-27 22:34:15 +0700

    Return a detail of movie

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -6,7 +6,18 @@ from django.http import JsonResponse
 def movie_list(request):
     movies = Movie.objects.all() # QuerySet
     data = {
-      'movies' : list(movies.values())  # Convert to PythonDict
+      'movies' : list(movies.values()), # Convert to PythonDict
     }
     
+    return JsonResponse(data)
+
+
+def movie_detail(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    data = {
+      'name': movie.name,
+      'description': movie.description,
+      'active': movie.active,
+    }
+
     return JsonResponse(data)
