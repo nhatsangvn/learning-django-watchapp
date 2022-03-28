d3f2552006d3d6ba89aa16f4c51feb9b897b9e0b
sangnn <sangnn@vng.com.vn>
2022-03-28 23:42:37 +0700

    Serializers/Func-basedView: add method get/post/put/delete

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -3,17 +3,44 @@ from rest_framework.decorators import api_view
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 
-@api_view()
+@api_view(['GET', 'POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True) ## serializer just process single objet
-    #print(dir(serializer))
+    
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        ## serializer just process single objet
+        serializer = MovieSerializer(movies, many=True)
+        #print(dir(serializer))
+        return Response(serializer.data)
 
-    return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+          
 
-
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def movie_detail(request, pk):
-    movie = Movie.objects.get(pk=pk)
-    serializer = MovieSerializer (movie)
-    return Response(serializer.data)
+
+    if request.method == 'GET':
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':    
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    if request.method == 'DELETE':
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(status=204)
+
