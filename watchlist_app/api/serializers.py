f4a2f52c0158843b6d122fee12712de7ed1e0d34
sangnn <sangnn@vng.com.vn>
2022-03-30 20:59:55 +0700

    Serializer: add 3 types of validators

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
+import re
+
+def special_chars(value):
+    if not re.search('^[a-zA-Z0-9]*$', value):
+        raise serializers.ValidationError('Name contains special characters!')
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators=[special_chars])
     description = serializers.CharField()
     active = serializers.BooleanField()
 
@@ -17,3 +22,12 @@ class MovieSerializer(serializers.Serializer):
         instance.save()
         return instance
 
+    def validate(self, obj):
+        if obj['name'] == obj['description']:
+            raise serializers.ValidationError("Name is the same as description!")
+        return obj
+
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("Movie name too short!")
+        return value
